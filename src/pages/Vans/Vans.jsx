070cf5fd39727275e8096be6d3d72f33e0431bba
Mark <muchirimark2@gmail.com>
2023-10-10 01:46:14 +0300

    Challenge ( done ) && Map
    - Fetched data from API
    - Mapped the data and styled them

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,6 +1,7 @@
+import './Vans.css';
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
-
+import { useEffect, useState } from 'react';
 /**
 * Challenge: Fetch and map over the data to display it on
 * the vans page. For an extra challenge, spend time styling
@@ -14,15 +15,64 @@ import Nav from "../../components/Nav/Nav";
 * 3. You may get an error saying "console.groupCollapsed is not
 *    a function". You can ignore it for now.
 */
-
 function Vans() {
+  // State to hold the fetched data
+  const [ data, setData ] = useState([]);
+
+  // useEffect to fetch data when the component mounts
+  useEffect(() => {
+    async function getVans() {
+      const res = await fetch('api/vans');
+      const data = await res.json();
+      setData(data.vans);
+    }
+    getVans();
+  }, []);
+
+  // Map through filter buttons
+  const filter = [ 'Simple', 'Luxury', 'Rugged' ];
+  const filterCards = filter.map((filterItem, index) => (
+    <div className="filter-button" key={index}>
+      <p>{filterItem}</p>
+    </div>
+  ));
+
+  // Map the data through cards
+  const vanElements = data.map(van => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <div className="van-price">
+          <p>${van.price}</p>
+          <span>/day</span>
+        </div>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
   return (
     <>
       <Nav />
-      <h1>Vans page goes here 🚐</h1>
+      <main className="vans-margin">
+        <h1>Explore our van options</h1>
+        {/* Filters */}
+        <div className="filters">
+          {filterCards}
+          <div className="clear-cont">
+            <p>Clear Filters</p>
+          </div>
+        </div>
+        {/* Vans data */}
+        <div className="van-list-container">
+          <div className="van-list">
+            {vanElements}
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );
 }
-
 export default Vans;
