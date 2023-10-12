bd5efba6228b432b1c46dbcc135d2e30897d0c19
Mark <muchirimark2@gmail.com>
2023-10-13 02:27:31 +0300

    Changed styles to be more responsice

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,13 +13,13 @@ function Home() {
 				<div className='homecontainer'>
 					<img className='homeimg' src='/vanlifehome.png' />
 					{/* Hero text */}
-						<p className='herotxt'>
-							You got the travel plans, we got the travel vans.
-						</p>
-						<p className='herosubtxt'>
-							Add adventure to your life by joining the #vanlife movement. Rent
-							the perfect van to make your perfect road trip.
-						</p>
+					<p className='herotxt'>
+						You got the travel plans, we got the travel vans.
+					</p>
+					<p className='herosubtxt'>
+						Add adventure to your life by joining the #vanlife movement. Rent
+						the perfect van to make your perfect road trip.
+					</p>
 					<Link to='/vans'>
 						<button className='findvanbutton'>
 							<p>Find your van</p>
