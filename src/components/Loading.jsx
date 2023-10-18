a68951a9d3c56ab2f5330191eecded12c9b31b65
Mark <muchirimark2@gmail.com>
2023-10-18 06:20:20 +0300

    styled loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,18 @@
-import './Loading.css'
+import "./Loading.css";
 import { FadingDots } from "react-cssfx-loading";
 
 function Loading() {
 	return (
 		<>
 			<div className='loading-container'>
-				<FadingDots color='#FF6B00' width='100px' height='100px' />
+				<div className=''>
+					<FadingDots
+						color='#FF6B00'
+						width='100px'
+						height='100px'
+					/>
+				</div>
+			<h2>...Loading...</h2>
 			</div>
 		</>
 	);
