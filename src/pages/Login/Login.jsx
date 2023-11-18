6f6e76ecb60d6d6213415d1b00c851a078d5b447
Mark <muchirimark2@gmail.com>
2023-11-18 05:10:02 +0300

    removed task comment

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,24 +3,6 @@ import { useState } from "react";
 import "./Login.css";
 import { loginUser } from "../../api";
 
-/**
- * Challenge: Code the sad path 🙁
- * 3. Add a `status` state and default it to "idle". When the
- *    login form begins submitting, set it to "submitting". When
- *    it's done submitting (whether successful or not), set it
- *    to "idle" again.
- * 4. Disable the button when the `status` state is "submitting"
- *    (this may require some Googling if you haven't done this
- *    before).
- * 5. Add an `error` state and default it to `null`. When the
- *    form is submitted, reset the errors to `null`. If there's
- *    an error from `loginUser` (add a .catch(err => {...}) in
- *    the promise chain), set the error to the error that
- *    comes back.
- * 6. Display the error.message below the `h1` if there's ever
- *    an error in state
- */
-
 function SignIn() {
 	const message = useLoaderData();
 	const [status, setStatus] = useState("idle");
