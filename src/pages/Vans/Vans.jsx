b37fd999559f38c526978a9ae8ed3be9da5819c3
Mark <muchirimark2@gmail.com>
2023-11-29 06:06:40 +0300

    changes:
      - Refactored the Await vans
      - Added the rendering function after:
        <Await resolve={dataPromise.vans}>{renderVanElements}</Await>

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,20 +1,16 @@
 // Import necessary CSS file and required hooks from React
 import "./Vans.css";
 import { useState } from "react";
-import { Link, useLoaderData, useSearchParams } from "react-router-dom";
+import { Await, Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 /**
- * Challenge: 
- * 1. Add `defer` to the loader.
- * 2. TBA
- * 
- * Reminder: 
- * - defer takes an object representing any data you
- *   want to have access to in the component. The value of the
- *   object property should be a promise (which `getVans()` is
- *   already returning.)
+ * Challenge: Implement Await.
+ * This code is a bit more complex than the simple Weather app, so
+ * there's a bit more to consider and shift around this time. Give
+ * it your best shot, and then we'll see a couple different ways
+ * we could accomplish this task.
  */
 
 function Vans() {
@@ -22,11 +18,10 @@ function Vans() {
 	// Using useSearchParams hook to access the URL search parameters
 	const [searchParams, setSearchParams] = useSearchParams();
 	// Loading data
-	const data = useLoaderData()
-	console.log(data)
+	const dataPromise = useLoaderData();
 	// Extracting the 'type' filter from the URL query parameters
 	const typeFilter = searchParams.get("type");
-	
+
 	// Function to handle changes in the filter parameters
 	//~ Compatible for multiple filter inputs
 	function handleFilterChange(key, value) {
@@ -59,55 +54,52 @@ filter item and create corresponding filter buttons */
 		</div>
 	));
 
-	// Filter function to display characters based on the filter
-	const displayedCharacters = typeFilter
-		? data.filter((char) => char.type.toLowerCase() === typeFilter)
-		: data;
-
-	// Map the data through cards and utilize lazy loading for the images
-	const vanElements = displayedCharacters.map((van) => (
-		<div key={van.id} className='van-tile'>
-			{/* Holds the url state for vanDetail back button */}
-			<Link
-				to={van.id}
-				state={{
-					search: `?${searchParams.toString()}`,
-					type: typeFilter,
-				}}
-			>
-				{/* Utilize LazyLoadImage component with necessary attributes */}
-				<LazyLoadImage
-					effect='blur'
-					src={van.imageUrl}
-					alt={van.name}
-					width={`100%`}
-				/>
-				<div className='van-info'>
-					<h3>{van.name}</h3>
-					<div className='van-price'>
-						<p>${van.price}</p>
-						<span>/day</span>
-					</div>
-				</div>
-				<div className='button-position'>
-					<i className={`van-type ${van.type} selected`}>{van.type}</i>
-				</div>
-			</Link>
-		</div>
-	));
-
-
 	// error handler
 	if (error) {
 		return <h1>There was an error: {error.message}</h1>;
 	}
 
-	return (
-		<>
-			<main className='vans-margin'>
-				<h1>Explore our van options</h1>
-				{/* Filters with dynamic display of active 
-				filters and clearing functionality */}
+	// Uses Await ( from React-Router-Dom ) to render the data
+	function renderVanElements(data) {
+		// Filter function to display characters based on the filter
+		const displayedCharacters = typeFilter
+			? data.filter((char) => char.type.toLowerCase() === typeFilter)
+			: data;
+
+		// Map the data through cards and utilize lazy loading for the images
+		const vanElements = displayedCharacters.map((van) => (
+			<div key={van.id} className='van-tile'>
+				{/* Holds the url state for vanDetail back button */}
+				<Link
+					to={van.id}
+					state={{
+						search: `?${searchParams.toString()}`,
+						type: typeFilter,
+					}}
+				>
+					{/* Utilize LazyLoadImage component with necessary attributes */}
+					<LazyLoadImage
+						effect='blur'
+						src={van.imageUrl}
+						alt={van.name}
+						width={`100%`}
+					/>
+					<div className='van-info'>
+						<h3>{van.name}</h3>
+						<div className='van-price'>
+							<p>${van.price}</p>
+							<span>/day</span>
+						</div>
+					</div>
+					<div className='button-position'>
+						<i className={`van-type ${van.type} selected`}>{van.type}</i>
+					</div>
+				</Link>
+			</div>
+		));
+
+		return (
+			<>
 				<div className='filters'>
 					{filterCards}
 					{typeFilter ? (
@@ -123,6 +115,17 @@ filter item and create corresponding filter buttons */
 				<div className='van-list-container'>
 					<div className='van-list'>{vanElements}</div>
 				</div>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<main className='vans-margin'>
+				<h1>Explore our van options</h1>
+				{/* Filters with dynamic display of active 
+				filters and clearing functionality */}
+				<Await resolve={dataPromise.vans}>{renderVanElements}</Await>
 			</main>
 		</>
 	);
