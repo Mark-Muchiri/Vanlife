22dd1357c27ad6e8af8bb52203453bc288bde536
Mark <muchirimark2@gmail.com>
2023-12-01 23:54:55 +0300

    Working on adding firebase to the project for backend functions

--- src/api.js
+++ src/api.js
@@ -1,3 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNmp27zQyq8-LPVC3NufwMZUt3DzPiU-M",
+  authDomain: "vanlife-mark.firebaseapp.com",
+  projectId: "vanlife-mark",
+  storageBucket: "vanlife-mark.appspot.com",
+  messagingSenderId: "1039330766406",
+  appId: "1:1039330766406:web:4d801e12775a3953242447"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
 export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : `/api/vans`;
   const res = await fetch(url);
